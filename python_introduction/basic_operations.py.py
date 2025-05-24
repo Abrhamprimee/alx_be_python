3144a2775fc2e4c29ed6bbcd83c4add022fa0b81
Abrhamprimee <Abrhamayalew@outlook.com>
2025-05-24 04:24:09 +0300

    Added python_introduction directory

---
+++ python_introduction/basic_operations.py.py
@@ -0,0 +1,14 @@
+pnumber1 = 10
+number2 = 5
+
+# Addition
+sum = number1 + number2
+print("sum of {0} and {1} is {2}".format(number1, number2, sum))
+
+# Subtraction
+difference = number1 - number2
+print("difference of {0} and {1} is {2}".format(number1, number2, difference))
+
+# Multiplication
+product = number1 * number2
+print("product of {0} and {1} is {2}".format(number1, number2, product))
