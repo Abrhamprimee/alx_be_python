1723959dcabae3cd14620723ec45e6484fc93772
Abrhamprimee <Abrhamayalew@outlook.com>
2025-05-24 04:58:32 +0300

    updated script

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,14 @@
-pnumber1 = 10
+number1 = 10  # Correcting the variable name
 number2 = 5
 
 # Addition
-sum = number1 + number2
-print("sum of {0} and {1} is {2}".format(number1, number2, sum))
+sum_result = number1 + number2  # Renaming 'sum' to avoid conflicts with the built-in function
+print("Sum of {0} and {1} is {2}".format(number1, number2, sum_result))
 
 # Subtraction
 difference = number1 - number2
-print("difference of {0} and {1} is {2}".format(number1, number2, difference))
+print("Difference of {0} and {1} is {2}".format(number1, number2, difference))
 
 # Multiplication
 product = number1 * number2
-print("product of {0} and {1} is {2}".format(number1, number2, product))
+print("Product of {0} and {1} is {2}".format(number1, number2, product))
